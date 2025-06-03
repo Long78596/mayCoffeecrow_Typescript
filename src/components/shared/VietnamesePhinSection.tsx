2ac865891032e5cb1510560f0035bcf89dc90b88
Thanh Long <longkolp16@gmail.com>
2025-06-03 17:47:13 +0700

    fix đoạn  banner

--- src/components/shared/VietnamesePhinSection.tsx
+++ src/components/shared/VietnamesePhinSection.tsx
@@ -1,8 +1,8 @@
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
+// import "swiper/css";
+// import "swiper/css/navigation";
 
 
 
